bcf9e5f9b811d966b48447d2ebfb162217dba50c
德振 <1391095970@qq.com>
2019-03-03 01:42:12 +0800

    feat(main): city link

---
+++ mail.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"golang.org/x/net/html/charset"
+	encoding2 "golang.org/x/text/encoding"
+	"golang.org/x/text/transform"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"regexp"
+)
+
+func main() {
+	resp, err := http.Get("https://www.zhenai.com/zhenghun")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Println("Error: status code:", resp.StatusCode)
+		return
+	}
+	e := determineEncoding(resp.Body)
+	utf8Reader := transform.NewReader(resp.Body, e.NewDecoder())
+	all, err := ioutil.ReadAll(utf8Reader)
+	if err != nil {
+		panic(err)
+	}
+	printCityList(all)
+}
+func determineEncoding(r io.Reader) encoding2.Encoding {
+	bytes, err := bufio.NewReader(r).Peek(1024)
+	if err != nil {
+		panic(err)
+	}
+	e, _, _ := charset.DetermineEncoding(bytes, "")
+	return e
+}
+func printCityList(contents []byte) {
+	re := regexp.MustCompile(`<a href="http://www.zhenai.com/zhenghun/[0-9a-z]+"[^>]*>[^<]+</a>`)
+	matches := re.FindAll(contents, -1)
+
+	for _, m := range matches {
+		fmt.Printf("%s\n", m)
+	}
+}
