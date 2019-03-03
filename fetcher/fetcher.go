8427f2292396632aef81a39c4e2c5a0e8b63eec9
德振 <dezhen98@gmail.com>
2019-03-03 21:55:07 +0800

    fix(fetchers):添加header头，防止有的网址反爬虫

--- fetcher/fetcher.go
+++ fetcher/fetcher.go
@@ -13,8 +13,14 @@ import (
 )
 
 func Fetch(url string) ([]byte, error) {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36")
 
-	resp, err := http.Get("https://www.zhenai.com/zhenghun")
+	resp, err := client.Do(req)
 	if err != nil {
 		panic(err)
 	}
