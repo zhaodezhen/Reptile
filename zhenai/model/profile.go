f76158e269904f590f39093b070640f7bbf6fe72
德振 <dezhen98@gmail.com>
2019-03-04 00:35:12 +0800

    feat(model):用户详情

---
+++ zhenai/model/profile.go
@@ -0,0 +1,14 @@
+package model
+
+type Profile struct {
+	Name          string
+	Marriage      string
+	Age           string
+	Constellation string
+	Height        string
+	Weight        string
+	Workplace     string
+	Income        string
+	Occupation    string
+	Education     string
+}
---
+++ zhenai/parser/profile.go
@@ -0,0 +1,38 @@
+package parser
+
+import (
+	"crawler/types"
+	"crawler/zhenai/model"
+	"log"
+	"src/github.com/PuerkitoBio/goquery"
+	"strings"
+)
+
+//获取用户详情
+func ParseProfile(contents []byte) types.ParseResult {
+	document, err := goquery.NewDocumentFromReader(strings.NewReader(string(contents)))
+	if err != nil {
+		log.Fatalln(err)
+	}
+	var user [10]string
+	document.Find(".purple").Each(func(i int, selection *goquery.Selection) {
+		user[i] = selection.Text()
+	})
+
+	profile := model.Profile{}
+	profile.Name = user[0]
+	profile.Marriage = user[1]
+	profile.Age = user[2]
+	profile.Constellation = user[3]
+	profile.Height = user[4]
+	profile.Weight = user[5]
+	profile.Workplace = user[6]
+	profile.Income = user[7]
+	profile.Occupation = user[8]
+	profile.Education = user[9]
+	result := types.ParseResult{
+		Items: []interface{}{profile},
+	}
+	return result
+}
+
