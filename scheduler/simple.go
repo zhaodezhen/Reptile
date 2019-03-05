b70189ce2947a4d95a37926b25fc55ab2b931843
德振 <dezhen98@gmail.com>
2019-03-06 00:59:53 +0800

    fix(simple): 使用goroutine解决worker循环等待造成的假死问题

--- scheduler/simple.go
+++ scheduler/simple.go
@@ -11,6 +11,5 @@ func (s *SimpleScheduler) ConfigureMasterWorkerChan(c chan engine.Request) {
 }
 
 func (s *SimpleScheduler) Submit(r engine.Request) {
-	// send request down to worker chan
-	s.workerChan <- r
+	go func() { s.workerChan <- r }()	//使用goroutine解决worker循环等待造成的假死问题
 }
